707ba1586d27f248eb51d3ba3ad830cd2a6f543d
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-01-25 21:08:16 +0100

    a whole lotta stuff

--- js/js.js
+++ js/js.js
@@ -1,4 +1,48 @@
-$(function(){
+$(function () {
 
-  
-})
+  $(".nav-button").on({
+    mouseenter: function () {
+      $(this).toggleClass("active")
+    },
+    mouseleave: function () {
+      $(this).toggleClass("active")
+    },
+    click: function (e) {
+      $(this).siblings().find(".sub-menu").hide();
+      $(this).find(".sub-menu").toggle();
+      e.stopPropagation();
+    }
+  });
+
+  $(".sub-menu p").on({
+    mouseenter: function () {
+      $(this).toggleClass("active")
+    },
+    mouseleave: function () {
+      $(this).toggleClass("active")
+    }
+  })
+
+  $(document).click(function () {
+    $(".sub-menu").hide();
+  })
+
+  $(".show-more").on({
+    mouseenter: function () {
+      $(this).toggleClass("active")
+    },
+    mouseleave: function () {
+      $(this).toggleClass("active")
+    },
+    click: function () {
+      $(this).siblings(".info").slideToggle(200);
+
+      if($(this).text() === "Show more"){
+        $(this).text("Show less")
+      } else {
+        $(this).text("Show more")
+      }
+    }
+  });
+
+});
