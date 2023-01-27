9fa036e08575a1c268c471f435a44ef274863229
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-01-27 11:39:22 +0100

    made the mobile version

--- js/js.js
+++ js/js.js
@@ -2,10 +2,10 @@ $(function () {
 
   $(".nav-button").on({
     mouseenter: function () {
-      $(this).toggleClass("active")
+      $(this).toggleClass("active");
     },
     mouseleave: function () {
-      $(this).toggleClass("active")
+      $(this).toggleClass("active");
     },
     click: function (e) {
       $(this).siblings().find(".sub-menu").hide();
@@ -16,33 +16,39 @@ $(function () {
 
   $(".sub-menu p").on({
     mouseenter: function () {
-      $(this).toggleClass("active")
+      $(this).toggleClass("active");
     },
     mouseleave: function () {
-      $(this).toggleClass("active")
+      $(this).toggleClass("active");
     }
   })
 
   $(document).click(function () {
     $(".sub-menu").hide();
+    $(".burger-submenu").hide();
   })
 
   $(".show-more").on({
     mouseenter: function () {
-      $(this).toggleClass("active")
+      $(this).toggleClass("active");
     },
     mouseleave: function () {
-      $(this).toggleClass("active")
+      $(this).toggleClass("active");
     },
     click: function () {
       $(this).siblings(".info").slideToggle(200);
 
       if($(this).text() === "Show more"){
-        $(this).text("Show less")
+        $(this).text("Show less");
       } else {
-        $(this).text("Show more")
+        $(this).text("Show more");
       }
     }
   });
+
+  $(".hamburger-button").on("click", function (e){
+    $(".burger-submenu").slideToggle(200);
+    e.stopPropagation();
+  })
 
 });
